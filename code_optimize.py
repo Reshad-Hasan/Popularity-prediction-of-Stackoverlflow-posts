465309e860efccc1a53d81aeb52eeaaabbb32f32
Reshad-Hasan <reshadhasan555@gmail.com>
2019-05-24 02:32:49 +0600

    data label complete not in saperate module, saperate module for label process may be better

--- code_optimize.py
+++ code_optimize.py
@@ -1,9 +1,14 @@
-feature_keys=['titleLen','bodyToTitle','userRep','codeLen','codeToBody','stopWordCount','nonStopWordCount','bodyLen','codeBlockCount','polarity','subjectivity','paraCount','tagCount','lowToUp','popularity']
+feature_keys=['titleLen','bodyToTitle','userRep','codeLen','codeToBody','stopWordCount',
+              'nonStopWordCount','bodyLen','codeBlockCount','polarity','subjectivity',
+              'paraCount','tagCount','lowToUp','popularity'
+              ]
 
 post_columns=['title','body','tagCount','reputation','popularity']
 
-processed_data_path='C:\\Users\\Reshad Hasan\\Desktop\\processed_data.csv'
+processed_data_path= 'C:\\Users\\Reshad Hasan\\Desktop\\processed_data.csv'
 
 csv_reading_path='C:\\Users\\Reshad Hasan\\Desktop\\temp.csv'
 
 ENCODING = 'utf-8'
+
+labeled_data_path='C:\\Users\\Reshad Hasan\\Desktop\\labeled_data.csv'
--- process-csv.py
+++ process-csv.py
@@ -1,50 +1,66 @@
 import csv
 from processPost import feature
 import time
-import  code_optimize
-start=time.time()
+import code_optimize
 
-inf=1000000
+start = time.time()
+
+inf = 1000000
+
+
 class post:
-    item=dict((i,None) for i in code_optimize.post_columns)
+    item = dict((i, None) for i in code_optimize.post_columns)
 
-    def __init__(self,item):
-        self.item=item
-        obj=feature(item)
-        self.exFeatures=obj.getFeatures()
+    def __init__(self, item):
+        self.item = item
+        obj = feature(item)
+        self.exFeatures = obj.getFeatures()
 
     def getFeatures(self):
         return self.exFeatures
 
 
-if __name__=='__main__':
-    with open(code_optimize.processed_data_path,mode='w',encoding=code_optimize.ENCODING) as write_file:
-        writer=csv.DictWriter(write_file,fieldnames=code_optimize.feature_keys)
+def set_lebel(mx, mn, featureValue):
+    lebel = 1
+    x = (mx - mn) / 5
+    mn+=x
+    while mn < int(featureValue):
+        lebel += 1
+        mn += x
+    return lebel
+
+
+if __name__ == '__main__':
+    mx = dict((i, -inf) for i in code_optimize.feature_keys)
+    mn = dict((i, inf) for i in code_optimize.feature_keys)
+    processed_feature_list = []
+    with open(code_optimize.processed_data_path, mode='w', encoding=code_optimize.ENCODING) as write_file:
+        writer = csv.DictWriter(write_file, fieldnames=code_optimize.feature_keys)
         writer.writeheader()
-        line_count=0
-
-        mx = dict((i,-inf) for i in code_optimize.feature_keys)
-        mn = dict((i, inf) for i in code_optimize.feature_keys)
-
-        with open(code_optimize.csv_reading_path,mode='r',encoding=code_optimize.ENCODING) as csv_file:
-            csv_reader=csv.DictReader(csv_file,delimiter=',')
-            line_count=0
+        line_count = 0
+        with open(code_optimize.csv_reading_path, mode='r', encoding=code_optimize.ENCODING) as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=',')
+            line_count = 0
             for row in csv_reader:
-                postObj=post(row)
-                exFeatures=postObj.getFeatures()
+                postObj = post(row)
+                exFeatures = postObj.getFeatures()
                 writer.writerow(exFeatures)
-                line_count+=1
+                processed_feature_list.append(exFeatures.copy())
+                line_count += 1
                 print(line_count)
                 for k in exFeatures.keys():
-                    mn[k]=min(mn[k],int(exFeatures[k]))
-                    mx[k]=max(mx[k],int(exFeatures[k]))
-            with open('C:\\Users\\Reshad Hasan\\Desktop\\min-max.txt','w',encoding=code_optimize.ENCODING) as file:
-                file.write('max values\n')
-                file.write(str(mx))
-                file.write('\nmin values\n')
-                file.write(str(mn))
-            print(mx)
-            print(mn)
+                    mn[k] = min(mn[k], int(exFeatures[k]))
+                    mx[k] = max(mx[k], int(exFeatures[k]))
 
-end=time.time()
-print(end-start)
+    with open(code_optimize.labeled_data_path, mode='w', encoding=code_optimize.ENCODING) as write_file:
+        writer = csv.DictWriter(write_file, fieldnames=code_optimize.feature_keys)
+        writer.writeheader()
+        feature_label = dict()
+        for row in processed_feature_list:
+            print(row)
+            for k in code_optimize.feature_keys:
+                feature_label[k] = set_lebel(mx[k], mn[k], row[k])
+            writer.writerow(feature_label)
+
+end = time.time()
+print(end - start)
